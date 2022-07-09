e991306c84a337a280c45c9624220079680339d2
Jonathan Teran <jonathan.nerat@gmail.com>
2022-07-09 20:06:53 -0300

    add array_gen type for array of void pointers

--- array.c
+++ array.c
@@ -37,3 +37,43 @@ void array_vec3_destroy(array_vec3 *arr) {
   free(arr->data);
   free(arr);
 }
+
+// For pointers
+
+array_gen *array_gen_init() {
+  array_gen *arr = malloc(sizeof(array_gen));
+
+  arr->size = 0;
+  arr->cap = ARRAY_INITIAL_CAPACITY;
+  arr->data = malloc(sizeof(void *) * arr->cap);
+
+  return arr;
+}
+
+bool array_gen_push(array_gen *arr, void *v) {
+  if (arr->size == arr->cap) {
+    void **data = realloc(arr->data, sizeof(void *) * arr->cap * 2);
+    if (!data)
+      return false;
+
+    if (data != arr->data)
+      arr->data = data;
+    arr->cap *= 2;
+  }
+
+  arr->data[arr->size++] = v;
+
+  return true;
+}
+
+void *array_gen_get(const array_gen *arr, size_t i) {
+  if (i >= arr->size)
+    return NULL;
+
+  return arr->data[i];
+}
+
+void array_gen_destroy(array_gen *arr) {
+  free(arr->data);
+  free(arr);
+}
--- array.h
+++ array.h
@@ -11,11 +11,20 @@ typedef struct {
   size_t cap;
 } array_vec3;
 
+typedef struct {
+  void **data;
+  size_t size;
+  size_t cap;
+} array_gen;
+
 array_vec3 *array_vec3_init();
-
 bool array_vec3_push(array_vec3 *arr, vec3 v);
-
 vec3 array_vec3_get(const array_vec3 *arr, size_t i);
-
 void array_vec3_destroy(array_vec3 *arr);
+
+array_gen *array_gen_init();
+bool array_gen_push(array_gen *arr, void *v);
+void *array_gen_get(const array_gen *arr, size_t i);
+void array_gen_destroy(array_gen *arr);
+
 #endif // ARRAY_H
